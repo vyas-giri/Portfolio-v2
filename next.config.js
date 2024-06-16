1f87fcc24f637e86f938115932c82a184f93fed9
Vyas Giri <vyasgiri25@gmail.com>
2024-06-17 01:42:23 +0530

    fixed some warnings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["cdn.sanity.io", "img.icons8.com"],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "img.icons8.com"
+            },
+            {
+                protocol: "https",
+                hostname: "cdn.sanity.io"
+            },
+        ]
     },
 }
 
